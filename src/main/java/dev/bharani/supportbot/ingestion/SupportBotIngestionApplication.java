8c2576e82e4670da3d48aed47217aa7997389140
bharanidharan_r <bharanivijay69@gmail.com>
2025-10-03 16:57:45 +0530

    Base project structure and added documents

---
+++ src/main/java/dev/bharani/supportbot/ingestion/SupportBotIngestionApplication.java
@@ -0,0 +1,13 @@
+package dev.bharani.supportbot.ingestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportBotIngestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportBotIngestionApplication.class, args);
+	}
+
+}
